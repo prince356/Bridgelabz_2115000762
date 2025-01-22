36f7d00f8f56e58c197ad3e8c67c4cef57db7612
prince356 <princechaurasia014@gmail.com>
2025-01-22 14:46:03 +0530

    Assignment2 added

---
+++ Assignment_2/que3.java
@@ -0,0 +1,7 @@
+public class que3{
+        public static void main(String []args){
+                double distanceInKms=10.8;
+                double distanceInMiles=10.8/1.6;
+                System.out.println("The distance "+distanceInKms+"kn in miles is "+distanceInMiles);
+        }
+}
